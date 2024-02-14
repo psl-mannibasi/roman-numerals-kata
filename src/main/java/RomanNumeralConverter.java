aa2b4037081308b1fecafa193e101f3bd031a25d
SSM <SSM@phoebussoftware.com>
2024-02-14 15:12:04 +0000

    use a better algorithm to calculate Numerals

--- src/main/java/RomanNumeralConverter.java
+++ src/main/java/RomanNumeralConverter.java
@@ -1,81 +1,60 @@
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class RomanNumeralConverter {
 
   HashMap<Integer, String> numeralMap = new HashMap<Integer, String>() {{
     put(1, "I");
+    put(2, "II");
+    put(3, "III");
+    put(4, "IV");
     put(5, "V");
+    put(6, "VI");
+    put(7, "VII");
+    put(8, "VIII");
+    put(9, "IX");
     put(10, "X");
+    put(20, "XX");
+    put(30, "XXX");
+    put(40, "XL");
     put(50, "L");
+    put(60, "LX");
+    put(70, "LXX");
+    put(80, "LXXX");
+    put(90, "XC");
     put(100, "C");
+    put(200, "CC");
+    put(300, "CCC");
+    put(400, "CD");
     put(500, "D");
+    put(600, "DC");
+    put(700, "DCC");
+    put(800, "DCCC");
+    put(900, "CM");
     put(1000, "M");
   }};
 
 
   public String convert(final Integer number) {
-    if (number == 0) {
-      return "";
+    if (number == 0 || number > 3999) {
+      throw new NumberFormatException("Cannot convert numbers lower than 1 or higher than 3999");
     }
-    int lowerMagnitudeNumber = number;
-    StringBuilder finalNumeral = new StringBuilder();
-    while (lowerMagnitudeNumber>0)
-    {
-      Integer key = roundDownToPowerOf10(lowerMagnitudeNumber);
-      String numeralCharacter = numeralMap.get(key);
-      // find nth power bounds of number eg if 121 the bounds are is 100 - 500 - 1000, or 93 bound are 10 - 50 - 100
-      // if number 30
-      // next nth power of number  * 10 = 100
-      int upperBound = key * 10;  // 100 is next nth power of number 30
-      int middleBound = upperBound / 2; // upperBound / 2 = 50
-      int lowerBound = key; // nth power of number e.g. 10th
 
-      finalNumeral.append(getNumeralCharacter(lowerMagnitudeNumber, lowerBound, middleBound, upperBound));
-
-      lowerMagnitudeNumber = lowerMagnitudeNumber % lowerBound;
+    String output = "";
+    String numberString = number.toString();
+    for (int i = 0; i < numberString.length(); i++) {
+      Integer num = Integer.valueOf(String.valueOf(numberString.charAt(i)));
+      int powerOf = numberString.length() - i - 1; // eg if number is 3 this will be 0, 20 will be 1, 500 will be 2, 1000 will be 3 etc
+      int powerOfNumber =  powerOf > 0 ? 1 * (int) Math.pow(10, powerOf) : num; // we only have number as a single digit so multiply it by the power of. eg num will be 2 for 200 so 1 * 10^2 = 1 * 100
+      int multiplier =  powerOf > 0 ? num : 1; // for numbers less than 9, we only need one numeral
+      if(num>0) output += repeatCharacterByNTimes(numeralMap.get(powerOfNumber), multiplier);
     }
-    return finalNumeral.toString();
+    return output;
   }
-
-  private String getNumeralCharacter(final Integer number, final int lowerBound, final int middleBound,
-      final int upperBound) {
-    // check number against bounds
-    if (number >= middleBound - lowerBound && number < middleBound) {
-      // e.g. if number is 100 check 40 - 49
-      return numeralMap.get(lowerBound) + numeralMap.get(middleBound);
-    } else if (number >= upperBound - lowerBound && number < upperBound) {
-      // e.g. if number is 100 check 90 - 99
-      return numeralMap.get(lowerBound) + numeralMap.get(upperBound);
-    } else if (number >= middleBound && number < middleBound + lowerBound) {
-      // e.g. if number is 100 check 50 - 59
-      return numeralMap.get(middleBound);
-    } else if (number >= middleBound + lowerBound && number < upperBound - lowerBound) {
-      // e.g. if number is 100 check 60-89
-      return numeralMap.get(middleBound) + repeatCharacterByNTimes(numeralMap.get(lowerBound),
-          (int) Math.floor(number - middleBound / lowerBound));
-    } else if (number < middleBound - lowerBound) {
-      // e.g. if number is 100 check 10 - 39
-      return repeatCharacterByNTimes(numeralMap.get(lowerBound), (int) Math.floor(number / lowerBound));
-    }
-     return "";
-  }
-
 
   private String repeatCharacterByNTimes(final String character, final Integer number) {
     return new String(new char[number]).replace("\0", character);
   }
 
-  // Thanks ChatGPT
-  public static int roundDownToPowerOf10(int number) {
-    if (number <= 0) {
-      throw new IllegalArgumentException("Number should be greater than 0");
-    }
-    int power = 1;
-    while (number >= 10) {
-      number /= 10;
-      power *= 10;
-    }
-    return power;
-  }
 
 }
